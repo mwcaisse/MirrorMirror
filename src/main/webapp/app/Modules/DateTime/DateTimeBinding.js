5d682ec0269606a23a0ba6d9889568ebc50efb92
Mitchell Caisse <computers1055@gmail.com>
2016-03-19 16:26:18 -0400

    Refactored the binding handlers a bit

--- src/main/webapp/app/Modules/DateTime/DateTimeBinding.js
+++ src/main/webapp/app/Modules/DateTime/DateTimeBinding.js
@@ -1,10 +1,8 @@
 "use strict";
 
-define("Modules/DateTime/DateTimeBinding", ["Service/koBindingHandlers", "AMD/koTemplateLoader!Modules/DateTime/DateTime.html"], function (handlers, templateName) {	
-	ko.bindingHandlers.dateTime = {
-		init: handlers.customTemplateInit,
-		update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {	
-			handlers.customTemplateUpdate(templateName, element, valueAccessor, allBindings, viewModel, bindingContext);
-		}
-	};	
+define("Modules/DateTime/DateTimeBinding", 
+		["Service/koBindingHandlers", "AMD/koTemplateLoader!Modules/DateTime/DateTime.html"], 
+		function (handlers, templateName) {	
+	
+	handlers.createBindingHandler("dateTime", templateName);
 });
--- src/main/webapp/app/Service/util.js
+++ src/main/webapp/app/Service/util.js
@@ -9,31 +9,34 @@ define("Service/util", [], function (text) {
 		 * 
 		 */
 		self.runEveryMs = function(func, ms) {
-			setInterval(func, ms);
+			return setInterval(func, ms);
 		};
 		
 		/** Runs the given function every x seconds
 		 *  @param func The function to run
 		 *  @param seconds The internval in seconds between each execution
+		 *  @return The interval ID 
 		 */
 		self.runEverySecond = function(func, seconds) {
-			self.runEveryMs(func, seconds * 1000);
+			return self.runEveryMs(func, seconds * 1000);
 		};
 		
 		/** Runs the given function every x minutes
 		 *  @param func The function to run
 		 *  @param minutes The internal in minutes between each execution
+		 *  @return The interval ID 
 		 */
 		self.runEveryMinute = function(func, minutes) {
-			self.runEverySecond(func, minutes * 60);
+			return self.runEverySecond(func, minutes * 60);
 		};
 		
 		/** Runs the given function every x hours
 		 * 	@param func The function to run
 		 *  @param hours The interval in hours between each execution
+		 *  @return The interval ID 
 		 */
 		self.runEveryHour = function(func, hours) {
-			self.runEveryMinute(func, hours * 60)
+			return self.runEveryMinute(func, hours * 60)
 		};
 		
 	})();
