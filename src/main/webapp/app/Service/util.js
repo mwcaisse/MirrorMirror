2b810a2365fcff4dcaf91b0bb5e15557e0376235
Mitchell Caisse <computers1055@gmail.com>
2016-03-19 20:49:24 -0400

    Added a interval type + interval type method to util

--- src/main/webapp/app/Service/util.js
+++ src/main/webapp/app/Service/util.js
@@ -5,6 +5,11 @@ define("Service/util", [], function (text) {
 	var util = new (function() {
 		var self = this;
 
+		self.UPDATE_INTERVAL_MS = "MS";
+		self.UPDATE_INTERVAL_SECOND = "SECOND";
+		self.UPDATE_INTERVAL_MINUTE = "MINUTE";
+		self.UPDATE_INTERVAL_HOUR = "HOUR";
+		
 		/** Runs the given function every x milliseconds
 		 * 
 		 */
@@ -38,6 +43,21 @@ define("Service/util", [], function (text) {
 		self.runEveryHour = function(func, hours) {
 			return self.runEveryMinute(func, hours * 60)
 		};
+		
+		self.runEvery = function (func, intervalType, interval) {
+			var ms = interval;
+			
+			switch (invervalType) {
+				case self.UPDATE_INTERVAL_HOUR:
+					ms *= 60; // 60 minutes in an hour
+				case self.UPDATE_INTERVAL_MINUTE:
+					ms *= 60; // 60 seconds in a minute
+				case self.UPDATE_INTERVAL_SECOND:
+					ms *= 1000; // 1000 ms in a second
+			}
+			
+			return self.runEveryMs(func, ms);			
+		}
 		
 	})();
 	
